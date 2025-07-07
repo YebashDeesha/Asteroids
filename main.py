04a41302930185f05cbe08429e3764c8269aac7f
Yebash Deesha <jan@pomaranski.pl>
2025-07-07 19:18:58 +0200

    pogrupowane_obiekty

--- main.py
+++ main.py
@@ -7,16 +7,24 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     dt = 0
     player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        updatable.update(dt)
 
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+
+        for d in drawable:
+            d.draw(screen)
+        
         pygame.display.flip()
         
 
